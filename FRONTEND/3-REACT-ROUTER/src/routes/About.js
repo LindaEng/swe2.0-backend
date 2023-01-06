19d3ab2fbf3f2c8978115f7014125fb96744bebf
= <90lineng@gmail.com>
2023-01-06 12:23:39 -0500

    react router dom demo complete

--- FRONTEND/3-REACT-ROUTER/src/routes/About.js
+++ FRONTEND/3-REACT-ROUTER/src/routes/About.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const About = () => {
     return (
         <div>
             <p>ABOUT COMPONENT</p>
+            <Link to='/'>Take me back home!</Link>
         </div>
     )
 }
--- FRONTEND/3-REACT-ROUTER/src/routes/Blog.js
+++ FRONTEND/3-REACT-ROUTER/src/routes/Blog.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Blog = () => {
     return (
         <div>
             <p> THIS IS THE BLOG COMPONENT</p>
+            <Link to='/'>Take me back home!</Link>
         </div>
     )
 }
